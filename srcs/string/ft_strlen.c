b78bc3654018ce6ae5e647a07eb7060e4e2d53ed
Eguelin <eguelin@student.42lyon.fr>
2025-01-30 16:37:18 +0100

    string: Optimized ft_strlen function for improved performance

--- srcs/string/ft_strlen.c
+++ srcs/string/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 09:01:56 by eguelin           #+#    #+#             */
-/*   Updated: 2025/01/29 18:45:40 by eguelin          ###   ########.fr       */
+/*   Updated: 2025/01/30 15:20:36 by eguelin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,29 @@
 
 size_t	ft_strlen(const char *s)
 {
-	size_t	i;
+	const char *start;
+	size_t		*qword_ptr;
+	size_t		qword;
+	size_t		i;
 
-	i = 0;
-	while (s[i] != 0)
-		i++;
-	return (i);
+	start = s;
+	i = (size_t)s % sizeof(size_t);
+	while (i > 0)
+	{
+		if (*s == 0)
+			return (s - start);
+		s++;
+		i--;
+	}
+	qword_ptr = (size_t *)s;
+	qword = *qword_ptr;
+	while (!((qword - 0x0101010101010101) & ~qword & 0x8080808080808080))
+	{
+		qword_ptr++;
+		qword = *qword_ptr;
+	}
+	s = (char *)qword_ptr;
+	while (*s)
+		s++;
+	return (s - start);
 }
